d464e2082113f90064757acaf558321211112c85
Marcos <mpineirotroncoso@danielcastelao.org>
2023-11-17 09:24:12 +0100

    javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Funciones"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Trabajador"}];updateSearchResults();
